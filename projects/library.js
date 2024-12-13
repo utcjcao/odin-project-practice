59d72508c93df2c21628f4825f824d9da1c59d94
utcjcao <chrisj.cao@mail.utoronto.ca>
2024-12-13 18:21:32 -0500

    i feel good about this library

--- projects/library.js
+++ projects/library.js
@@ -4,18 +4,31 @@ class Library {
   }
 
   add() {
+    if (this.library.length === 14) {
+      const errorSpan = document.getElementById("error");
+      errorSpan.textContent = "too many books!";
+      return;
+    } else {
+      const errorSpan = document.getElementById("error");
+      errorSpan.textContent = "";
+    }
     const titleInput = document.getElementById("title");
     const authorInput = document.getElementById("author");
-    const book = new Book(titleInput.value, authorInput.value);
+    const book = new Book(
+      titleInput.value,
+      authorInput.value,
+      crypto.randomUUID()
+    );
     titleInput.value = "";
     authorInput.value = "";
     this.library.push(book);
+    console.log(this.library.length);
     this.draw();
   }
 
   delete(book) {
     function bookFilter(val) {
-      return !(book.title === val.title && book.author === val.author);
+      return !(book.id === val.id);
     }
     this.library = this.library.filter(bookFilter);
     this.draw();
@@ -28,14 +41,20 @@ class Library {
       const bookDiv = document.createElement("div");
       bookDiv.classList.add("book-container");
       const titleSpan = document.createElement("span");
-      titleSpan.textContent = "title: " + book.title;
+      titleSpan.textContent = "title: ";
+      const titleContentSpan = document.createElement("span");
+      titleContentSpan.textContent = book.title;
       const authorSpan = document.createElement("span");
-      authorSpan.textContent = "author: " + book.author;
+      authorSpan.textContent = "author: ";
+      const authorContentSpan = document.createElement("span");
+      authorContentSpan.textContent = book.author;
       const deleteButton = document.createElement("button");
       deleteButton.textContent = "delete book";
       deleteButton.addEventListener("click", () => this.delete(book));
       bookDiv.appendChild(titleSpan);
+      bookDiv.appendChild(titleContentSpan);
       bookDiv.appendChild(authorSpan);
+      bookDiv.appendChild(authorContentSpan);
       bookDiv.appendChild(deleteButton);
 
       libraryDiv.appendChild(bookDiv);
@@ -44,9 +63,10 @@ class Library {
 }
 
 class Book {
-  constructor(title, author) {
+  constructor(title, author, id) {
     this.title = title;
     this.author = author;
+    this.id = id;
   }
 }
 
